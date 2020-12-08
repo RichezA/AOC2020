33a31390c0406bec695cdb841ba59deb843c9e21
Antoine <richezatr@gmail.com>
2020-12-08 18:34:14 +0100

    added part2

--- day8/Program.cs
+++ day8/Program.cs
@@ -12,15 +12,18 @@ namespace day8
 
         static Dictionary<int, string> instructions = new Dictionary<int, string>();
 
+        static bool isFinished = false;
+
         static void Main(string[] args)
         {
-            var template = File.ReadAllLines("input.txt");
+            var template = File.ReadAllLines("test.txt");
             foreach (var item in template.Select((value, index) => (value, index)))
             {
                 instructions.Add(item.index, item.value);
             }
 
             System.Console.WriteLine($"Part1: Acc number is : {Part1()}");
+            System.Console.WriteLine($"Part2: Acc number is : {Part2()}");
         }
 
         static int Part1()
@@ -29,13 +32,43 @@ namespace day8
             {
                 ProcessInstruction(instructions.First());
             }
+
             return acc;
         }
 
+        static int Part2()
+        {
+            isFinished = false;
+            acc = 0;
+            foreach (var item in instructions)
+            {
+                if (isFinished)
+                {
+                    break;
+                }
+
+                var copy = instructions.Clone();
+
+                switch (copy[item.Key].Split()[0])
+                {
+                    case "nop":
+                        copy[item.Key] = copy[item.Key].Replace("nop", "jmp");
+                        ProcessInstruction(copy.First());
+                        break;
+                    case "jmp":
+                        copy[item.Key] = copy[item.Key].Replace("jmp", "nop");
+                        ProcessInstruction(copy.First());
+                        break;
+                }
+            }
+
+            return acc;
+        }
         static void ProcessInstruction(KeyValuePair<int, string> instruction)
         {
-            if (done.Contains(instruction.Key))
+            if (instruction.Key == instructions.Count - 1 || done.Contains(instruction.Key) || isFinished)
             {
+                isFinished = true;
                 return;
             }
 
@@ -58,6 +91,21 @@ namespace day8
 
             done.Add(instruction.Key);
             ProcessInstruction(instructions.ElementAt(instruction.Key + next));
+        }
+    }
+
+    static class DictionaryHelpers
+    {
+        public static Dictionary<int, string> Clone(this Dictionary<int, string> origin)
+        {
+            var clone = new Dictionary<int, string>(origin.Count, origin.Comparer);
+
+            foreach (var item in origin)
+            {
+                clone.Add(item.Key, (string)item.Value.Clone());
+            }
+
+            return clone;
         }
     }
 }
